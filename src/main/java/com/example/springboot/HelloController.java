a2d9276c9320ee605b523ac7d2bb316b0f2fc034
Robert Abeyta <arobert@vmware.com>
2023-11-16 10:22:05 -0700

    making a change

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,13 +1,17 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.logging.Logger;
 
 @RestController
 public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
+		System.out.println("hit /");
 		return "Greetings from Spring Boot + Tanzu!";
 	}
 
